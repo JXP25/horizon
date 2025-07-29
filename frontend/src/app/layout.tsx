b3f20a78be6491b92b2ca9907e533670e1e2fe28
JaiPannu-IITI <pannujai06iiti@gmail.com>
2025-07-29 23:58:07 +0530

    Disable service worker

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
       >
         <ApolloWrapper>
           {children}
-          <ServiceWorkerRegister />
+          {/* <ServiceWorkerRegister /> */}
         </ApolloWrapper>
       </body>
     </html>
